5f24ff682cc73dcf1531bc3e6371d7fa812059fe
hayk.inanc@gmail.com <hayk.inanc@gmail.com>
2019-12-24 00:32:34 +0300

    Create sqlCode.sql

---
+++ sqlCode.sql
@@ -0,0 +1,26 @@
+--Табельный номер , имя, отдел , зарплата
+
+Create table PersonA(
+id number primary key, 
+name varchar2(20), 
+dep number, 
+sal number); 
+
+
+Insert into PersonA(id,name,dep,sal) values(1, 'Аня',10,9000);
+Insert into PersonA(id,name,dep,sal) values(2, 'Саша',10,5500);
+Insert into PersonA(id,name,dep,sal) values(3, 'Таня',10,7000);
+Insert into PersonA(id,name,dep,sal) values(4, 'Ваня',20,2300);
+Insert into PersonA(id,name,dep,sal) values(5, 'Олег',20,4300);
+Insert into PersonA(id,name,dep,sal) values(6, 'Коля',20,3900);
+Insert into PersonA(id,name,dep,sal) values(7, 'Таня',30,7000);
+Insert into PersonA(id,name,dep,sal) values(8, 'Макс',30,9000);
+Insert into PersonA(id,name,dep,sal) values(9, 'Таня',30,8500);
+Insert into PersonA(id,name,dep,sal) values(10,'Макс',30,9900);
+Insert into PersonA(id,name,dep,sal) values(11,'Олег',30,9900);
+Insert into PersonA(id,name,dep,sal) values(12,'Макс',30,9900);
+Insert into PersonA(id,name,dep,sal) values(13,'Макс',30,9900);
+Insert into PersonA(id,name,dep,sal) values(14,'Макс',30,9900);
+Insert into PersonA(id,name,dep,sal) values(15,'Макс',30,9900);
+Insert into PersonA(id,name,dep,sal) values(16,'Макс',30,7000);
+Insert into PersonA(id,name,dep,sal) values(17,'Таня',30,3500);
