7d6031ba6db815e409e951dd93d10e0d6e218bbf
hayk.inanc@gmail.com <hayk.inanc@gmail.com>
2019-12-24 00:46:28 +0300

    Update sqlCode2.sql

--- sqlCode2.sql
+++ sqlCode2.sql
@@ -4,18 +4,18 @@ name varchar2(20),
 pay number); 
 
 
-Insert into PersonA(id, name, pay) values(1,  'Макс',    9000);
-Insert into PersonA(id, name, pay) values(2,  'Макс',    5500);
-Insert into PersonA(id, name, pay) values(3,  'Макс',    7000);
-Insert into PersonA(id, name, pay) values(4,  'Макс',    2300);
-Insert into PersonA(id, name, pay) values(5,  'Макс',    4300);
-Insert into PersonA(id, name, pay) values(6,  'Леонид',  3900);
-Insert into PersonA(id, name, pay) values(7,  'Леонид',  7000);
-Insert into PersonA(id, name, pay) values(8,  'Леонид',  9000);
-Insert into PersonA(id, name, pay) values(9,  'Леонид',  8500);
-Insert into PersonA(id, name, pay) values(10, 'Леонид',  9900);
-Insert into PersonA(id, name, pay) values(11, 'Николай', 9900);
-Insert into PersonA(id, name, pay) values(12, 'Николай', 9900);
-Insert into PersonA(id, name, pay) values(13, 'Николай', 9900);
-Insert into PersonA(id, name, pay) values(14, 'Николай', 9900);
-Insert into PersonA(id, name, pay) values(15, 'Николай', 9900);
+Insert into Payouts(id, name, pay) values(1,  'Макс',    9000);
+Insert into Payouts(id, name, pay) values(2,  'Макс',    5500);
+Insert into Payouts(id, name, pay) values(3,  'Макс',    7000);
+Insert into Payouts(id, name, pay) values(4,  'Макс',    2300);
+Insert into Payouts(id, name, pay) values(5,  'Макс',    4300);
+Insert into Payouts(id, name, pay) values(6,  'Леонид',  3900);
+Insert into Payouts(id, name, pay) values(7,  'Леонид',  7000);
+Insert into Payouts(id, name, pay) values(8,  'Леонид',  9000);
+Insert into Payouts(id, name, pay) values(9,  'Леонид',  8500);
+Insert into Payouts(id, name, pay) values(10, 'Леонид',  9900);
+Insert into Payouts(id, name, pay) values(11, 'Николай', 9900);
+Insert into Payouts(id, name, pay) values(12, 'Николай', 9900);
+Insert into Payouts(id, name, pay) values(13, 'Николай', 9900);
+Insert into Payouts(id, name, pay) values(14, 'Николай', 9900);
+Insert into Payouts(id, name, pay) values(15, 'Николай', 9900);
